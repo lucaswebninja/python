ba205689ae5478ef17dcd1a3cf5c942493342827
Lucas Nogueira de Azevedo <127260919+lucaswebninja@users.noreply.github.com>
2023-03-18 15:21:30 -0300

    Add files via upload

---
+++ import this.py
@@ -0,0 +1,9 @@
+import this
+
+nome = input('Qual seu nome?: ')
+rg = int(input('Qual seu RG?: '))
+cpf = int(input('Qual seu CPF?:'))
+telefone = int(input('Qual é seu telefone?: '))
+
+print('Seu nome é: ', nome, 'seu rg é:', rg, 'seu cpf é:', cpf, 'seu telefone é:', telefone)
+
